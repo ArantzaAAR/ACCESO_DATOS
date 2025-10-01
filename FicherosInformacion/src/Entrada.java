9dc6b3beacab7df121f9635513c6e3bab01307ec
Arantza <arantzaalcazar@gmail.com>
2025-10-01 13:58:35 +0200

    Subo cambios de la clase 30-09

--- FicherosInformacion/src/Entrada.java
+++ FicherosInformacion/src/Entrada.java
@@ -1,10 +1,15 @@
+import java.io.File;
+
 public class Entrada {
 
     public static void main(String[] args) {
         Operaciones operaciones= new Operaciones();
         //operaciones.informacionFichero (  "src/resources/ejemplo1.txt" );
-        operaciones.mostrarFicherosRecurrentes( "src\\resources");
+        //operaciones.mostrarFicherosRecurrentes( new File( "Acceso_Datos\\Ejercicios_clase\\FicherosInformacion"));
         //operaciones.informacionDirectorio (  "src/resources/cosas/para/analizar" );
         // operaciones.escribirFichero (  "src/resources/escritura.txt" );
+        //operaciones.cifrarMensaje("src/resources/escritura.txt");
+        //operaciones.escrituraSuperior("src/resources/escrituraSuperior.txt");
+        operaciones.exportarUsuario("src/resources/Usuarios.csv");
     }
 }
---
+++ FicherosInformacion/src/model/Usuario.java
@@ -0,0 +1,59 @@
+package model;
+
+public class Usuario {
+    private int id;
+    private String nombre, apellido, dni, correo;
+
+    public Usuario(int id, String nombre, String apellido, String correo, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.dni = dni;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format( "%d, %s, %s, %s, %s", id, nombre, apellido, dni, correo);
+    }
+}
